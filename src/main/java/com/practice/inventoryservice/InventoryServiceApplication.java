f9ac8e1327c1be335cf6513cf0bd23970d207e8a
mehdi <mehdi-h@outlook.fr>
2023-01-16 17:13:12 +0100

    Used case eureka

--- src/main/java/com/practice/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/practice/inventoryservice/InventoryServiceApplication.java
@@ -4,8 +4,10 @@ import com.practice.inventoryservice.services.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication implements CommandLineRunner {
     private final ProductService productService;
 
